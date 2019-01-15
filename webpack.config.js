fbf69ec43de77594d8ac9febf7122f1b92fe5c64
Zach Posten <zposten@gmail.com>
2019-01-15 13:33:44 -0600

    feat: Add webpack-dev-server to serve files
    This now replaces http-server in the project.
    
    Note that the webpack-dev-server also takes care of running the webpack
    process and creating the bundle file, but unlike just running 'webpack',
    the webpack-dev-server serves the files from memory instead of from the
    file system.
    
    Webpack-dev-server also has automatic reloading built in, so when you
    make a change to a source file (that webpack manages), you will not have
    to refresh the browser manually.

--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,17 @@ module.exports = {
     path: path.resolve(__dirname, 'app/dist'),
     filename: 'app.bundle.js',
   },
+  // Documented at: https://webpack.js.org/configuration/dev-server/
+  devServer: {
+    // The root that webpack-dev-server serves files from.  The default is 
+    // the directory of this webpack config file.
+    contentBase: path.resolve(__dirname, 'app'),
+    // The webpack bundled files will be available to the browser under this
+    // path (when served from memory by the webpack-dev-server).  This allows
+    // the import in index.html to be the same for webpack and webpack-dev-server.
+    publicPath: '/dist/',
+    // Tell webpack-dev-server to watch files that are not built by webpack
+    // but are inside of the content base.
+    watchContentBase: true,
+  }
 }
